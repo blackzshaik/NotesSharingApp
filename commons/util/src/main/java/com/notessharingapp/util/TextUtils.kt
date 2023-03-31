af73a9605b6ea238b8c3a02b93f7fdf936ea1bed
mytaskboard <mytaskboardapp@gmail.com>
2023-03-31 10:55:49 +0530

    Remove ignored files

--- commons/util/src/main/java/com/notessharingapp/util/TextUtils.kt
+++
@@ -1,5 +0,0 @@
-package com.notessharingapp.util
-
-import android.util.Patterns
-
-fun CharSequence?.isValidEmail() = !isNullOrEmpty() && Patterns.EMAIL_ADDRESS.matcher(this).matches()
--- core/data/src/main/java/com/notessharingapp/data/CollectionNames.kt
+++
@@ -1,7 +0,0 @@
-package com.notessharingapp.data
-
-object CollectionNames {
-
-    const val USER = "user"
-    const val NOTES = "notes"
-}
